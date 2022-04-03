3db9e465b8d21f69d53e202adf3a794d9486c3ae
WEREMSOFT <pablo.weremczuk@gmail.com>
2022-04-03 15:57:28 +0200

    Calling C function from C#10

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.InteropServices;
+
+[DllImport("libTest.so", EntryPoint = "printText")]
+static extern void printTest(string text);
+
+Console.WriteLine("Hello, World!");
+printTest("SARLANGA!!!");
+
---
+++ cFunction.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void printText(const char *str)
+{
+    printf("Hello from C!!! %s\n", str);
+}
